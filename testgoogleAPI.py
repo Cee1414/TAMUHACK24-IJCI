a8319357cbcb9180642776f15c71197e652c1480
soup-scott <scottc@LAPTOP-GV7O37P9.localdomain>
2024-01-28 05:57:44 -0600

    y

--- testgoogleAPI.py
+++ testgoogleAPI.py
@@ -1,29 +1,43 @@
 import requests
 import json
+import urllib.parse
 
 
-# f = open("airports.json", "w")
-# response = requests.get("https://americanairlines-e02d72e53739.herokuapp.com/airports/all")
-# if response.status_code == 200:
-#     f.write(response.text)
-# else:
-#     print(response.status_code)
+f = open("airports.json", "w")
+response = requests.get("https://americanairlines-e02d72e53739.herokuapp.com/airports/all")
+if response.status_code == 200:
+    f.write(response.text)
+else:
+    print(response.status_code)
+f.close()
 
-with open('airports.json', 'r') as file:
-    data_array = json.load(file)
+f = open("airports.json", "r")
+airports = json.load(f)
+f.close()
 
+originsWithCoordinates = {}
 
-
-OriginsWithCoordinates = {}
-
-for flight in data_array:
-    flightName = flight['code']
-    latitude = flight['location']['latitude']
-    longitude = flight['location']['longitude']
-    OriginsWithCoordinates[flightName] = [latitude, longitude]
+for airport in airports:
+    airportName = airport['code']
+    latitude = airport['location']['latitude']
+    longitude = airport['location']['longitude']
+    originsWithCoordinates[airportName] = [latitude, longitude]
     
 
-print("All Coordinates:", OriginsWithCoordinates)
+print("All Coordinates:", originsWithCoordinates)
+
+slice1 = "https://maps.googleapis.com/maps/api/distancematrix/json?destinations="
+slice2 = "&origins="
+slice3 = "&key=/mnt/c/Users/camso/CSCE221/googleAPIkey.txt"
+
+for originKey in originsWithCoordinates:
+    originCoords = f'{originsWithCoordinates[originKey][0]} {originsWithCoordinates[originKey][1]}'
+    
+    for destinationKey in originsWithCoordinates:
+        destinationCoords = f'{originsWithCoordinates[destinationKey][0]} {originsWithCoordinates[destinationKey][1]}'
+
+
+
 
 # response = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json?destinations=New%20York%20City%2C%20NY&origins=Washington%2C%20DC&units=imperial&key=AIzaSyDOEnQPZxu06yhdUz6Kv3OiUV3fHPCNPbY")
 # if response.status_code == 200:
